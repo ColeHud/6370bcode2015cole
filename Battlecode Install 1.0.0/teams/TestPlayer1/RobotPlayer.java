06115e7ff281b89483cf0c196b55b6ff64dfc00a
ColeHud <cmhudson11@gmail.com>
2015-01-05 22:56:22 -0500

    Added in battlecode

---
+++ Battlecode Install 1.0.0/teams/TestPlayer1/RobotPlayer.java
@@ -0,0 +1,20 @@
+/**
+ * @author Cole Hudson
+ * @version 1/5/2015
+ */
+
+package TestPlayer1;
+
+import battlecode.common.*;
+import java.util.*;
+
+public class RobotPlayer 
+{
+	public static void run(RobotController rc)
+	{
+		while(true)
+		{
+			rc.yield();
+		}
+	}
+}
